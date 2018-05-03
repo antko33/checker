50e9c23202e1806a5d767ac343c6e65364f1045b
Фтещт <antko33@gmail.com>
2018-05-03 23:16:09 +0300

    Добавлен класс TaskItem

---
+++ PeremServer/TaskItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PeremServer
+{
+    class TaskItem
+    {
+        private string path;
+        FileStream file;
+
+        public TaskItem(string path)
+        {
+            this.path = path;
+        }
+
+        public TaskItem(FileStream fs)
+        {
+            file = fs;
+        }
+
+        public FileStream GetFileStream()
+        {
+            if (file != null) return file;
+            file = new FileStream(path, FileMode.Open);
+            return file;
+        }
+    }
+}
